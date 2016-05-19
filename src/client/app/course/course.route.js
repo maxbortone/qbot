f4081c70b1b98828741579460ed391689399f4d6
Max Bortone <maxbortone@gmail.com>
2016-05-19 20:19:27 +0200

    fixed module name for course and resources routes

--- src/client/app/course/course.route.js
+++ src/client/app/course/course.route.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app.login')
+        .module('app.course')
         .run(appRun);
 
     appRun.$inject = ['routerHelper'];
--- src/client/app/resources/resources.route.js
+++ src/client/app/resources/resources.route.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app.login')
+        .module('app.resources')
         .run(appRun);
 
     appRun.$inject = ['routerHelper'];
